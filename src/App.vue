3dd1f82dbb733d2c93c18b788f73f76eecce88ac
CodeCuaTienNe <tiendmse182896@fpt.edu.vn>
2024-05-23 10:43:07 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LoginForm from "./components/Login/LoginForm.vue";
+</script>
+
+<template>
+  <LoginForm msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Login/Register.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script setup>
+export default {};
+</script>
+
+<style>
+</style>
