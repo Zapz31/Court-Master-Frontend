42bbd879dce09b7aa6b9c9ee4617664cad5de665
CodeCuaTienNe <tiendmse182896@fpt.edu.vn>
2024-06-08 14:42:35 +0700

    Merge code with new change from back-end: Adjust the registration form and navigate to the login page

--- src/components/Register/RegisterForm.vue
+++ src/components/Register/RegisterForm.vue
@@ -105,11 +105,31 @@
             {{ confirmPasswordError }}
           </p>
         </label>
-
+        <p
+          v-if="duplicateError === 'Email or phone number has already exsit'"
+          class="error"
+        >
+          {{ duplicateError }}
+        </p>
         <h3 v-if="formError" class="error">{{ formErrorMessage }}</h3>
 
         <button class="submit">Submit</button>
-        <p class="signin">Already have an acount ? <a href="#">Signin</a></p>
+        <!-- <p class="signin">Already have an acount ? <a href="#">Signin</a></p> -->
+        <div>
+          <p
+            class="signin"
+            v-if="
+              duplicateError === 'Email or phone number has already exsit' ||
+              duplicateError === ''
+            "
+          >
+            Already have an acount ? <a href="#">Signin</a>
+          </p>
+          <p class="signin" v-else>
+            Registered successfully, sign in here:
+            <router-link to="/login">Signin</router-link>
+          </p>
+        </div>
       </form>
     </div>
   </div>
@@ -118,7 +138,9 @@
 <script setup>
 import axios from "axios";
 import { computed, ref } from "vue";
+import { RouterLink } from "vue-router";
 
+const duplicateError = ref("");
 const firstNameError = ref("");
 const lastNameError = ref("");
 const phoneNumberError = ref("");
@@ -211,7 +233,7 @@ const registerUser = async () => {
 
   try {
     const response = await axios.post(
-      "http://localhost:8080/courtmaster/users/register",
+      "http://localhost:8080/courtmaster/auth/register",
       {
         userId: 3,
         firstName: firstName.value,
@@ -223,6 +245,14 @@ const registerUser = async () => {
       }
     );
     console.log(response.data);
+
+    if (
+      response.data.duplicateError === "Email or phone number has already exsit"
+    ) {
+      duplicateError.value = "Email or phone number has already exsit";
+    } else {
+      duplicateError.value = "login success";
+    }
   } catch (error) {
     console.error(error);
   }
@@ -373,7 +403,3 @@ const registerUser = async () => {
   }
 }
 </style>
-
-
-
-
